0e7a8f15bac366d0085d419992727aa8b08c1303
Nikolaj Majorov <nikolaj@majorov.biz>
2019-04-03 19:25:06 +0200

    change to query param

--- src/main/java/org/acme/ws/ClustomerService.java
+++ src/main/java/org/acme/ws/ClustomerService.java
@@ -11,8 +11,9 @@ import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
+//import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -62,8 +63,7 @@ public class ClustomerService {
 
     @DELETE
     @Transactional
-    @Path("/customers/{id}")
-    public Response delete(@PathParam("id") Integer id) {
+    public Response delete(@QueryParam("id") Integer id) {
         
         Customer entity =  entityManager.getReference(Customer.class,id);
         if (entity == null) {
