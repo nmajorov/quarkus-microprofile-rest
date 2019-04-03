e6a45c9bdd6e0efaf03c9040b7149654e039c5de
Nikolaj Majorov <nikolaj@majorov.biz>
2019-04-03 18:59:35 +0200

    add import sql

---
+++ src/main/resources/import.sql
@@ -0,0 +1,4 @@
+INSERT INTO customers(ID, NAME) VALUES (1, 'FC Servette Geneve')
+INSERT INTO customers(ID, NAME) VALUES (2, 'FC Herta Berlin')
+INSERT INTO customers(ID, NAME) VALUES (3, 'FC Hansa Rostock')
+INSERT INTO customers(ID, NAME) VALUES (4, 'FC Bayern Muenchen')
