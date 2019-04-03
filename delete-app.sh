77370dcd9c2c168ce2c63e1dd316c54b95796927
Nikolaj Majorov <nikolaj@majorov.biz>
2019-04-03 16:44:15 +0200

    add uber jar option

---
+++ delete-app.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+oc delete all --selector app=quarkus-microprofile-rest
+
+
--- ose-create-app.sh
+++ ose-create-app.sh
@@ -10,14 +10,14 @@ echo $SCRIPT_DIR
 oc new-app java \
    --code=$SCRIPT_DIR \
    --env=AB_JOLOKIA_OFF=true \
-   --env=MAVEN_CLEAR_REPO=true \
    --name='quarkus-microprofile-rest'
-
+   #--env=JAVA_APP_JAR='quarkus-microprofile-rest-1.0-SNAPSHOT-runner.jar'
+   
 #oc process  -f eap/eap64-basic-s2i.json -p APPLICATION_NAME=helloworld -p SOURCE_REPOSITORY_URL=https://github.com/jboss-developer/jboss-eap-quickstarts -p SOURCE_REPOSITORY_REF=6.4.x -p CONTEXT_DIR=helloworld | oc create -n yourproject -f -
 
 
 
-oc start-build quarkus-microprofile-rest --from-repo=$SCRIPT_DIR
+#oc start-build quarkus-microprofile-rest --from-repo=$SCRIPT_DIR
 
 sleep 10
 
